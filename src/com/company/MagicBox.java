7090b5ef1ab3bcbbc7fdcc8a42f314de2a032d28
lyubovbrnsk <brnsssskl@gmail.com>
2023-03-20 00:29:35 +0500

    Init commit

---
+++ src/com/company/MagicBox.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.Random;
+
+public class MagicBox<T> {
+
+    Object [] arr;
+
+    public MagicBox(int col){
+        arr=new Object[col];
+    }
+
+
+    public boolean add(T item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                arr[i] = item;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T pick(){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                throw new RuntimeException("Коробка не полна. Oсталось заполнить " + (arr.length - i) + " ячеек");
+            }
+        }
+            Random random = new Random();
+            int randomInt = random.nextInt(arr.length); // джава подберёт случайное число от 0 до ЧИСЛО невключительно
+        return (T) arr[randomInt];
+    }
+
+
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MagicBox<String> magicBox= new MagicBox<String>(3);
+        magicBox.add("Hello");
+        magicBox.add("World");
+//        magicBox.pick();
+
+        magicBox.add("!");
+        magicBox.pick();
+        String p = magicBox.pick();
+        System.out.println(p);
+
+        MagicBox<Integer> magicBox1 = new MagicBox<Integer>(3);
+        magicBox1.add(123);
+        magicBox1.add(456);
+//        magicBox.pick();
+
+        magicBox1.add(789);
+        magicBox1.pick();
+        Integer p1 = magicBox1.pick();
+        System.out.println(p1);
+    }
+}
